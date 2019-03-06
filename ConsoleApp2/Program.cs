566553d54f65b28bdb1e9f90a72375d95ef3fe20
visualstudio <visualstudio@122-PC15>
2019-03-06 12:14:27 -0500

    lab 13 commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,43 +11,40 @@ namespace ConsoleApp2
 //Assignment 2
 // March 6,2019
 {
-    using System;
-    delegate void ExampleDelegate(String xyz);
-    class program
+    public class program
     {
-        public static void Method1(string xyz)
-        {
-            Console.WriteLine(xyz + "Method1");
-        }
-        public static void Method2(string xyz)
-        {
-            Console.WriteLine(xyz + "Method2");
-        }
         public static void Main()
         {
-            ExampleDelegate ex1Delegate,ex2Delegate,ex3Delegate, myDelegate;
-
-            ex1Delegate = new ExampleDelegate(Method1);
-            ex1Delegate = new ExampleDelegate(Method2);
-            ex3Delegate = ex1Delegate + ex2Delegate;
-            myDelegate = ex1Delegate + ex2Delegate;
-            ex1Delegate("aaa");
-            ex2Delegate("bbb");
-            ex3Delegate("ccc");
-            myDelegate = ex1Delegate - ex2Delegate;
-            myDelegate("eee");
-            myDelegate = ex1Delegate - ex2Delegate
-            myDelegate("fff");
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
         }
     }
-   
+    public delegate int MyDelegate(out int i);
+    public class DelegateExercises
+    {
+        int Method1(out int i)
+        {
+        System.Console.WriteLine("Method1");
+            i=10;
+            return 0;
+        }
+    }
+    public void Method3()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        MyDelegate myDelegate1 = new MyDelegate(Method1);
+        MyDelegate myDelegate2 = myDelegate + myDelegate1;
+        int intValue;
+        myDelegate2(out intValue);
+    }
+        
+       
 
+
+
+    
 }
-
-
-
-
 
 
 
