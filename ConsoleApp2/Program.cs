c6e2214148340fdb978da9b4d639b35654b00288
visualstudio <visualstudio@122-PC15>
2019-03-06 11:50:56 -0500

    lab  11 commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,31 +11,18 @@ namespace ConsoleApp2
 //Assignment 2
 // March 6,2019
 {
-    public class program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-
-    }
+  
     public delegate void MyDelegate();
 
     public class DelegateExercises
     {
 
 
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
+       
         public void Method3()
         {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
+         
+            System.Console.WriteLine(MyDelegate.ToString());
         }
 
 
