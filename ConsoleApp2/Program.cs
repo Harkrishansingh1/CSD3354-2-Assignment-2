b7eaae2662e08342ef399881f7decc266628246d
visualstudio <visualstudio@122-PC15>
2019-03-06 09:57:08 -0500

    Initial Commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //student Name -Harkrishan Singh Student ID C0732442
+            //Studnt Name- Jatinder Singh student ID C0727454
+            //CSD3354 Section 2
+            //Assignment 2
+            // March 6,2019
+
         }
     }
 }
