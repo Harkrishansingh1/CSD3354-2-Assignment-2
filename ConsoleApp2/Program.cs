5cf0901aa228c30a106ced0218430283575784f7
visualstudio <visualstudio@122-PC15>
2019-03-06 11:10:58 -0500

    lab03 commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,44 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
+//student Name -Harkrishan Singh Student ID C0732442
+//Studnt Name- Jatinder Singh student ID C0727454
+//CSD3354 Section 2
+//Assignment 2
+// March 6,2019
 {
     class program
     {
         static void Main(string[] args)
         {
-            DelegateExercises a = new DelegateExercises();
-            a.Method3();
-            Console.ReadLine();
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
         }
 
     }
 
     public class DelegateExercises
     {
-        //student Name -Harkrishan Singh Student ID C0732442
-        //Studnt Name- Jatinder Singh student ID C0727454
-        //CSD3354 Section 2
-        //Assignment 2
-        // March 6,2019
-        public delegate int MyDelegate(int intValue);
-        public int Method1(int intMethod1)
+       
+        public delegate int MyDelegate();
+        void Method1()
         {
-            return intMethod1 * 2;
-        }     
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
+            System.Console.WriteLine("MyDelegate");
         }
         public void Method3()
         {
-            MyDelegate mydelegate = new MyDelegate(Method1);
-            int result1 = mydelegate(10);
-            System.Console.WriteLine(result1);
-            mydelegate = new MyDelegate(Method2);
-            int result2 = mydelegate(10);
-            System.Console.WriteLine(result2);
-            
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
+        
+            
+        
 
 
     }
